7e90f9bcd5ed4b3a627b7c87a98e23a731834c4c
Hunaid Memon <hunaid.hanif@gmail.com>
2022-01-28 12:50:32 +0500

    create API end point

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../models/User');
 
@@ -39,7 +41,18 @@ router.post('/', [
 
         await user.save();
 
-        res.send('User created in MongoDB');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'),{
+            expiresIn: 3600000,
+        }, (err, token) => {
+            if(err) throw err;
+            res.json({ token })
+        })
 
     } catch(err) {
         console.log(err.message);
